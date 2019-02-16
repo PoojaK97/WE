fc78253f02bc6943dd9ca871e3878481845e1f90
PoojaK97 <thisispooja97@gmail.com>
2019-02-16 14:40:12 +0530

    Added Splash Screen

---
+++ app/src/main/java/com/womandroid/we/SplashScreen.java
@@ -0,0 +1,37 @@
+package com.womandroid.we;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class SplashScreen extends AppCompatActivity {
+
+    private int SLEEP_TIMER = 3;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Log.d("splash","splash screen invoked");
+        setContentView(R.layout.activity_splash_screen);
+        LogoLauncher logoLauncher = new LogoLauncher();
+        logoLauncher.start();
+
+    }
+
+    private class LogoLauncher extends Thread {
+
+        public void run(){
+
+            try{
+                sleep(1000 * SLEEP_TIMER);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            Intent intent = new Intent(SplashScreen.this,MainActivity.class);
+            startActivity(intent);
+            SplashScreen.this.finish();
+        }
+    }
+}
